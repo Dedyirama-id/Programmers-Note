703de7f854bd18a348537195c7fe8e2f3a553279
Dedy Irama <dedyirama.ars@gmail.com>
2024-05-22 13:55:01 +0700

    Refactoring graph.h

--- src/header/graph.h
+++ src/header/graph.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "linkedlist.h"
+#include "app.h"
 
 using namespace std;
 
@@ -25,23 +25,66 @@ namespace gr {
     Vertex<T> *prev = nullptr;
     Edge<T> *edgeList = nullptr;
 
-    Vertex(T data) : data(data) {}
+    Vertex(T data, unsigned int id) : data(data), id(id) {}
+    ~Vertex() {
+      if (edgeList != nullptr) {
+        Edge<T> *toDelete = edgeList;
+        while (toDelete != nullptr) {
+          edgeList = edgeList->next;
+          delete toDelete;
+          toDelete = edgeList;
+        }
+        edgeList = nullptr;
+      }
+    }
+
+    Edge<T> *searchEdgeById(unsigned int id) {
+      Edge<T> *current = edgeList;
+      while (current != nullptr) {
+        if (current->vertexRef->id == id) {
+          return current;
+        }
+        current = current->next;
+      }
+      return nullptr;
+    }
 
     bool addEdgeByVertex(Vertex<T> *vertex) {
       if (edgeList == nullptr) {
         edgeList = new Edge<T>(vertex);
         return true;
       }
-      
+
       Edge<T> *newEdge = new Edge<T>(vertex);
       newEdge->next = edgeList;
       edgeList->prev = newEdge;
       edgeList = newEdge;
+      newEdge->prev = nullptr;
       return true;
     }
 
     bool addEdgeByVertexId(unsigned int id) {
-      return addEdgeByVertex(searchById(id));
+      Vertex<T> *vertex = searchEdgeById(id);
+      if (vertex == nullptr) return false;
+      return addEdgeByVertex(vertex);
+    }
+
+    bool deleteEdge(Edge<T> *edge) {
+      if (edge == nullptr) return false;
+      if (edge->prev != nullptr) {
+        edge->prev->next = edge->next;
+      }
+      if (edge->next != nullptr) {
+        edge->next->prev = edge->prev;
+      }
+      delete edge;
+      return true;
+    }
+
+    bool deleteEdgeById(unsigned int id) {
+      Edge<T> *edge = searchEdgeById(id);
+      if (edge == nullptr) return false;
+      return deleteEdge(edge);
     }
   };
 
@@ -54,7 +97,7 @@ namespace gr {
     string filePath;
 
     Graph() : vertices(nullptr), vertexCount(0), edgeCount(0) {}
-    Graph(string filePath) : vertices(nullptr), vertexCount(0), edgeCount(0) {}
+    Graph(string filePath) : vertices(nullptr), vertexCount(0), edgeCount(0), filePath(filePath) {}
 
     ~Graph() {}
 
@@ -74,8 +117,8 @@ namespace gr {
       return true;
     }
 
-    bool addVertex(T data) {
-      Vertex<T> *newVertex = new Vertex<T>(data);
+    bool addVertex(T data, unsigned int id) {
+      Vertex<T> *newVertex = new Vertex<T>(data, id);
       return addVertex(newVertex);
     }
 
@@ -90,8 +133,22 @@ namespace gr {
       return nullptr;
     }
 
+    bool destroyRelationshipToVertex(unsigned int id) {
+      Vertex<T> *current = vertices;
+      while (current != nullptr) {
+        Edge<T> *connectedEdge = current->searchEdgeById(id);
+        if (connectedEdge != nullptr) {
+          current->deleteEdge(connectedEdge);
+        }
+        current = current->next;
+      }
+      return true;
+    }
+
     bool deleteVertexByPtr(Vertex<T> *vertex) {
       if (vertex == nullptr) return false;
+      if (vertex->edgeList != nullptr) destroyRelationshipToVertex(vertex->id);
+
       if (vertex->prev != nullptr) {
         vertex->prev->next = vertex->next;
       }
@@ -108,21 +165,29 @@ namespace gr {
       return deleteVertexByPtr(vertex);
     }
 
-    bool addEdgeByID(unsigned int sourceID, unsigned int destID) {
-      Vertex<T> *sourceVertex = searchById(sourceID);
+    bool addEdgeById(unsigned int sourceId, unsigned int destID) {
+      Vertex<T> *sourceVertex = searchById(sourceId);
       Vertex<T> *destVertex = searchById(destID);
       if (sourceVertex == nullptr || destVertex == nullptr) return false;
-      sourceVertex->edgeList = new Edge<T>(destVertex);
+      sourceVertex->addEdgeByVertex(destVertex);
       return true;
     }
 
     void displayGraph() {
       Vertex<T> *current = vertices;
       while (current != nullptr) {
-        cout << current->data << " ";
+        cout << "[" << current->id << "] " << current->data << " -> ";
+        Edge<T> *currentEdge = current->edgeList;
+        while (currentEdge != nullptr) {
+          cout << currentEdge->vertexRef->id;
+          if (currentEdge->next != nullptr) {
+            cout << ", ";
+          }
+          currentEdge = currentEdge->next;
+        }
+        cout << endl;
         current = current->next;
       }
-      cout << endl;
     }
   };
 }
