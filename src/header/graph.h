b91f4f4f24015210a95d7cde9508a7512de7584c
Dedy Irama <dedyirama.ars@gmail.com>
2024-05-22 11:40:23 +0700

    Rebuild graph.h file

--- src/header/graph.h
+++ src/header/graph.h
@@ -11,72 +11,118 @@ namespace gr {
   template <typename T>
   struct Edge {
     Vertex<T> *vertexRef;
-    Edge<T> *next;
+    Edge<T> *prev = nullptr;
+    Edge<T> *next = nullptr;
 
     Edge(Vertex<T> *vertexRef) : vertexRef(vertexRef), next(nullptr) {}
   };
 
   template <typename T>
-  struct Vertex : public ll::Node<T> {
-    ll::LinkedList<Edge<T>> *edgeList;
+  struct Vertex {
+    unsigned int id;
+    T data;
+    Vertex<T> *next = nullptr;
+    Vertex<T> *prev = nullptr;
+    Edge<T> *edgeList = nullptr;
 
-    Vertex(T data) : ll::Node<T>(data), edgeList(new ll::LinkedList<Edge<T>>()) {}
+    Vertex(T data) : data(data) {}
 
-    void add2WayEdgeByVertex(Vertex<T> *vertex) {
-      edgeList->addBack(Edge<T>(vertex));
+    bool addEdgeByVertex(Vertex<T> *vertex) {
+      if (edgeList == nullptr) {
+        edgeList = new Edge<T>(vertex);
+        return true;
+      }
+      
+      Edge<T> *newEdge = new Edge<T>(vertex);
+      newEdge->next = edgeList;
+      edgeList->prev = newEdge;
+      edgeList = newEdge;
+      return true;
+    }
+
+    bool addEdgeByVertexId(unsigned int id) {
+      return addEdgeByVertex(searchById(id));
     }
   };
 
   template <typename T>
-  class Graph : public ll::LinkedList<T> {
+  class Graph {
   public:
+    Vertex<T> *vertices;
+    int vertexCount;
+    int edgeCount;
     string filePath;
 
-    Graph(string filePath) : filePath(filePath) {}
+    Graph() : vertices(nullptr), vertexCount(0), edgeCount(0) {}
+    Graph(string filePath) : vertices(nullptr), vertexCount(0), edgeCount(0) {}
 
-    bool checkIfEdgeExistByID(unsigned int sourceVID, unsigned int destVID) {
-      if (this->isEmpty()) return false;
+    ~Graph() {}
 
-      Vertex<T> *sourceNode = this->searchById(sourceVID);
-      if (!sourceNode) return false;
-      Vertex<T> *source = &(sourceNode->data);
-      return source->edgeList->searchById(destVID) != nullptr;
-    }
+    bool isEmpty() { return vertices == nullptr; }
 
-    bool addEdgeByID(unsigned int sourceVID, unsigned int destVID) {
-      ll::Node<Vertex<T>> *sourceNode = this->searchById(sourceVID);
-      ll::Node<Vertex<T>> *destNode = this->searchById(destVID);
-
-      if (!sourceNode || !destNode) return false;
-
-      Vertex<T> *sourceVertex = &(sourceNode->data);
-      Vertex<T> *destVertex = &(destNode->data);
-
-      if (!checkIfEdgeExistByID(sourceVID, destVID)) {
-        sourceNode->data.edgeList->addBack(Edge<T>(destVertex));
-        destNode->data.edgeList->addBack(Edge<T>(sourceVertex));
+    bool addVertex(Vertex<T> *newVertex) {
+      if (isEmpty()) {
+        vertices = newVertex;
+        vertexCount++;
         return true;
       }
-      return false;
+
+      newVertex->next = vertices;
+      vertices->prev = newVertex;
+      vertices = newVertex;
+      vertexCount++;
+      return true;
     }
 
     bool addVertex(T data) {
-      this->addBack(Vertex<T>(data));
+      Vertex<T> *newVertex = new Vertex<T>(data);
+      return addVertex(newVertex);
+    }
+
+    Vertex<T> *searchById(int id) {
+      Vertex<T> *current = vertices;
+      while (current != nullptr) {
+        if (current->id == id) {
+          return current;
+        }
+        current = current->next;
+      }
+      return nullptr;
+    }
+
+    bool deleteVertexByPtr(Vertex<T> *vertex) {
+      if (vertex == nullptr) return false;
+      if (vertex->prev != nullptr) {
+        vertex->prev->next = vertex->next;
+      }
+      if (vertex->next != nullptr) {
+        vertex->next->prev = vertex->prev;
+      }
+      delete vertex;
+      return true;
+    }
+
+    bool deleteVertexById(unsigned int id) {
+      Vertex<T> *vertex = searchById(id);
+      if (vertex == nullptr) return false;
+      return deleteVertexByPtr(vertex);
+    }
+
+    bool addEdgeByID(unsigned int sourceID, unsigned int destID) {
+      Vertex<T> *sourceVertex = searchById(sourceID);
+      Vertex<T> *destVertex = searchById(destID);
+      if (sourceVertex == nullptr || destVertex == nullptr) return false;
+      sourceVertex->edgeList = new Edge<T>(destVertex);
       return true;
     }
 
     void displayGraph() {
-      Vertex<T> *vertexNode = this->getHead();
-      while (vertexNode != nullptr) {
-        cout << vertexNode->data.data << " -> ";
-        Edge<T> *edgeNode = vertexNode->data.edgeList->getHead();
-        while (edgeNode != nullptr) {
-          cout << edgeNode->data.vertexRef->data << " "; // Akses langsung data dari vertexRef
-          edgeNode = edgeNode->next;
-        }
-        cout << endl;
-        vertexNode = vertexNode->next;
+      Vertex<T> *current = vertices;
+      while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
       }
+      cout << endl;
     }
   };
 }
