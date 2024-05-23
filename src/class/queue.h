e5f193fef45a9dc9541ecd770c515234b17cb17d
Dedy Irama <dedyirama.ars@gmail.com>
2024-05-23 10:45:32 +0700

    Added queue.h

---
+++ src/class/queue.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "linkedlist.h"
+
+using namespace std;
+using namespace ll;
+
+namespace qu {
+  template <typename T>
+  class Queue : protected LinkedList<T> {
+  public:
+    Queue() : LinkedList<T>() {}
+    ~Queue() {}
+
+    T peek() {
+      return this->getHead()->data;
+    }
+
+    bool isEmpty() {
+      return this->getHead() == nullptr;
+    }
+
+    void enqueue(T data) {
+      this->addBack(data);
+    }
+
+    bool dequeue() {
+      return this->removeFront();
+    }
+  };
+}
